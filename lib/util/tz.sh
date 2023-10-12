dcf73372a182de10b879164442588b502d172b45
widhisec <39062772+widhisec@users.noreply.github.com>
2023-10-12 07:40:27 +0700

    Add files via upload

---
+++ lib/util/tz.sh
@@ -0,0 +1,16 @@
+gans(){
+       echo ""
+    #    curl -s -k -X $'POST' \
+    #  -H $'Host: i.instagram.com' \
+    # -H $'X-Ig-App-Locale: in_ID' \
+    #  -H $'X-Ig-Device-Locale: en_US' \
+    #  -H $'X-Ig-Mapped-Locale: id_ID' \
+    #  -H $'User-Agent: Instagram 265.0.0.19.301 Android (28/9; 240dpi; 720x1280; OnePlus; ONEPLUS A5000; OnePlus5; qcom; en_US)' \
+    #  -H $'Accept-Language: id-ID' \
+    #  -H $'Authorization: Bearer IGT:2:eyJkc191c2VyX2lkIjoiMjI5NTI0NzE2MzEiLCJzZXNzaW9uaWQiOiIyMjk1MjQ3MTYzMSUzQTR3VGF0ajBRSUxFRGw2JTNBMTklM0FBWWZlZm9WVDJnVXVHRVNzNHNTSHZjckpfbkRvREh4Y3RUSkd2NGhhZ3cifQ=='  \
+    #   --data-urlencode "enc_old_password=#PWD_INSTAGRAM_BROWSER:0:$(this height):${psss}" \
+    #   --data-urlencode "enc_new_password1=#PWD_INSTAGRAM_BROWSER:0:$(this height):jfeofijeoifjewfjj" \
+    #   --data-urlencode "enc_new_password2=#PWD_INSTAGRAM_BROWSER:0:$(this height):jfeofijeoifjewfjj" \
+    #  $'https://i.instagram.com/api/v1/accounts/change_password/'
+   }
+gans
